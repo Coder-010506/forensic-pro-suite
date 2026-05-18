1892e54cb78dec5a4b887974dc88a88416d52288
Tamanna Rath <tamannarath01@gmail.com>
2026-05-18 20:29:44 +0530

    feat: implement EXIF metadata and geospatial tags extraction pipeline

--- Server/main.py
+++ Server/main.py
@@ -31,7 +31,6 @@ ALLOWED_EXTENSIONS = {
 
 MAX_FILE_SIZE = 500 * 1024 * 1024
 
-
 @app.get("/api/stats")
 async def get_case_stats():
     try:
@@ -104,6 +103,7 @@ async def run_forensic_pipeline(file: UploadFile = File(...)):
             "modified": report['metadata']['modified'],
             "accessed": report['metadata']['accessed'],
             "permissions": report['metadata']['permissions'],
+            "exif_metadata": report['metadata']['exif'],
             "magic_signature": report['metadata']['magic_signature'],
             "threat_level": report['threat_level'],
             "status": "COMPLETED",
